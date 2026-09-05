8af3c2a9e1be379599aa2d36bdc9f1673b927796
JaderoChan <c_dl_cn@outlook.com>
2026-09-05 14:01:13 +0800

    feat: add tweak field for version

--- backend/version.cpp
+++ backend/version.cpp
@@ -15,24 +15,31 @@ throw std::invalid_argument("invalid version string '" + std::string(string_view
     if (pos1 == std::string_view::npos) THROW_INVALID_VERSION_STRING(str);
     const std::size_t pos2 = str.find('.', pos1 + 1);
     if (pos2 == std::string_view::npos) THROW_INVALID_VERSION_STRING(str);
+    const std::size_t pos3 = str.find('.', pos2 + 1);
 
 #undef THROW_INVALID_VERSION_STRING
 
     std::string_view majorStr = str.substr(0, pos1);
     std::string_view minorStr = str.substr(pos1 + 1, pos2 - pos1 - 1);
-    std::string_view patchStr = str.substr(pos2 + 1);
+    std::string_view patchStr = str.substr(
+        pos2 + 1,
+        (pos3 == std::string_view::npos ? str.size() : pos3) - pos2 - 1);
+    std::string_view tweakStr = (pos3 == std::string_view::npos ? "" : str.substr(pos3 + 1));
     const int major = std::stoi(std::string(majorStr));
     const int minor = std::stoi(std::string(minorStr));
     const int patch = std::stoi(std::string(patchStr));
+    const int tweak = tweakStr.empty() ? 0 : std::stoi(std::string(tweakStr));
 
     if ((major >= 0 && major <= UINT8_MAX) &&
         (minor >= 0 && minor <= UINT8_MAX) &&
-        (patch >= 0 && patch <= UINT8_MAX))
+        (patch >= 0 && patch <= UINT8_MAX) &&
+        (tweak >= 0 && tweak <= UINT8_MAX))
     {
         return Version(
             static_cast<unsigned char>(major),
             static_cast<unsigned char>(minor),
-            static_cast<unsigned char>(patch));
+            static_cast<unsigned char>(patch),
+            static_cast<unsigned char>(tweak));
     }
     else
     {
@@ -48,5 +55,6 @@ std::string Version::toString() const
     return
         std::to_string(static_cast<int>(major)) + "." +
         std::to_string(static_cast<int>(minor)) + "." +
-        std::to_string(static_cast<int>(patch));
+        std::to_string(static_cast<int>(patch)) + "." +
+        std::to_string(static_cast<int>(tweak));
 }
--- backend/version.hpp
+++ backend/version.hpp
@@ -6,32 +6,35 @@
 struct Version
 {
     static_assert(sizeof(unsigned char) == 1, "sizeof(unsigned char) != 1");
-    static_assert(sizeof(unsigned int)  >= 3, "sizeof(unsigned int) < 3");
+    static_assert(sizeof(unsigned int)  == 4, "sizeof(unsigned int) != 4");
 
-    Version() : major(0), minor(0), patch(0) {}
-    Version(unsigned char major, unsigned char minor, unsigned char patch)
-        : major(major), minor(minor), patch(patch) {}
+    constexpr Version() noexcept : major(0), minor(0), patch(0), tweak(0) {}
+    constexpr Version(
+        unsigned char major, unsigned char minor,
+        unsigned char patch, unsigned char tweak = 0) noexcept
+        : major(major), minor(minor), patch(patch), tweak(tweak) {}
 
     /**
-     * 从 "major.minor.patch" 格式的字符串中解析版本号。
+     * 从 "major.minor.patch"/"major.minor.patch.tweak" 格式的字符串中解析版本号。
      *
      * @throw invalid_argument
      */
     static Version fromString(std::string_view str);
 
-    /** 将版本号序列化为 "major.minor.patch" 格式的字符串。 */
+    /** 将版本号序列化为 "major.minor.patch.tweak" 格式的字符串。 */
     std::string toString() const;
 
     /** 获取版本号的哈希值，同时可用于比较版本大小。 */
     unsigned int hash() const
     {
         return
-            (static_cast<unsigned int>(major) << 16) |
-            (static_cast<unsigned int>(minor) << 8)  |
-            (static_cast<unsigned int>(patch));
+            (static_cast<unsigned int>(major) << 24) |
+            (static_cast<unsigned int>(minor) << 16) |
+            (static_cast<unsigned int>(patch) << 8)  |
+            (static_cast<unsigned int>(tweak));
     }
 
-    unsigned char major, minor, patch;
+    unsigned char major, minor, patch, tweak;
 };
 
 static inline bool operator==(const Version& lhs, const Version& rhs)
